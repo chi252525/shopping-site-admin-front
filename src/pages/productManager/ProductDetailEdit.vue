194ae6fb2260c97d870c92ee0fcdf02f5ebabe34
chi252525 <chi252525@hotmail.com.tw>
2025-01-02 17:25:39 +0800

    update

--- src/pages/productManager/ProductDetailEdit.vue
+++ src/pages/productManager/ProductDetailEdit.vue
@@ -11,29 +11,96 @@
         <div class="q-mt-md">
           <q-card flat bordered>
             <q-card-section>
-              <div class="text-h6">Basic</div>
+              <div class="text-h6">基本資訊</div>
             </q-card-section>
-            <q-card-section class="q-pt-none">
-              <template>
-                <div class="q-pa-md">
-                  <q-uploader
-                    label="Auto Uploader"
-                    auto-upload
-                    url="http://localhost:4444/upload"
-                    multiple
-                  />
-                </div>
-              </template>
-            </q-card-section>
-            <q-card-section class="q-pt-none">
+
+            <q-card-section class="q-pt-md">
               <q-input
                 v-model="inputVal"
                 outlined
-                hint="說明"
-                label="姓名"
-                class="is_require"
+                label="baseSku"
+                class="is_require q-mt-md"
               >
               </q-input>
+              <q-input
+                v-model="inputVal"
+                outlined
+                label="name"
+                class="is_require q-mt-md"
+              >
+              </q-input>
+              <q-input
+                v-model="inputVal"
+                outlined
+                label="成本"
+                class="is_require q-mt-md"
+              >
+              </q-input>
+              <q-input
+                v-model="inputVal"
+                outlined
+                label="售價"
+                class="is_require q-mt-md"
+              >
+              </q-input>
+              <q-input
+                v-model="inputVal"
+                outlined
+                label="優惠價"
+                class="is_require q-mt-md"
+              >
+              </q-input>
+              <q-select
+                v-model="inputVal"
+                :options="options"
+                outlined
+                placeholder="大分類"
+                class="q-mt-md"
+                :rules="[(val) => val.length === 10]"
+              />
+              <q-select
+                v-model="inputVal"
+                :options="options"
+                outlined
+                placeholder="小分類"
+                class="q-mt-md"
+                :rules="[(val) => val.length === 10]"
+              />
+              <q-select
+                v-model="inputVal"
+                outlined
+                :options="options"
+                label="開始時間"
+                class="q-mt-md"
+                placeholder="Placeholder"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="event" color="primary" />
+                </template>
+              </q-select>
+              <q-select
+                v-model="inputVal"
+                outlined
+                :options="options"
+                label="結束時間"
+                class="q-mt-md"
+                placeholder="Placeholder"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="event" color="primary" />
+                </template>
+              </q-select>
+
+              <q-select
+                v-model="inputVal"
+                :options="options"
+                outlined
+                label="單位"
+                placeholder="單位"
+                class="q-mt-md"
+                :rules="[(val) => val.length === 10]"
+              />
+              <q-checkbox v-model="inputVal" val="line" label="是否有庫存" />
             </q-card-section>
 
             <q-separator inset />
@@ -44,15 +111,21 @@
             </q-card-section>
           </q-card>
         </div>
+
         <div class="q-mt-md">
           <q-card flat bordered>
-            <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-            </q-card-section>
+            <q-card-section class="q-pt-md">
+              <div class="text-h6">基本資訊</div>
 
-            <q-card-section class="q-pt-none">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <q-select
+                v-model="inputVal"
+                :options="options"
+                outlined
+                label="選擇模板"
+                placeholder="選擇模板"
+                class="q-mt-md"
+                :rules="[(val) => val.length === 10]"
+              />
             </q-card-section>
 
             <q-separator inset />
@@ -70,5 +143,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const inputVal = ref('');
+const options = ref(['第一層分類', '第二層分類', '第三層分類']);
 </script>
 <style scoped></style>
